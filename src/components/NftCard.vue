4f7fe6053efa822a825f2b81d7153fc8e9b21076
kaanfkennedy <dev.kaanfkennedy@gmail.com>
2022-02-06 22:07:12 +0300

    ADD COMPONENTS

---
+++ src/components/NftCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <w-card :image="nft.image">
+        {{ nft.name }}
+        <div class="spacer"></div>
+        {{ nft.description }}
+    </w-card>
+</template>
+
+
+<script>
+export default {
+    name: 'NftCard',
+
+    props: {
+        nft: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
---
+++ src/components/counter.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <h1>{{ counter }}</h1>
+        <w-flex justify-center align-center >
+            <w-button class="ma2 white" bg-color="black" @click="this.increment()">Increment</w-button>
+            <w-button class="ma2 white" bg-color="black" @click="this.decrement()">Decrement</w-button>
+        </w-flex>
+        <p id="warningMessage"></p>
+        <w-button id="mint" class="ma3">Mint!</w-button>
+    </div>
+</template>
+
+<script>
+import { mint } from '../Web3Client';
+
+export default {
+    name: 'Counter',
+    data() {
+        return {
+            counter: 0
+        }
+    },
+
+    methods: {
+        increment,
+        decrement,
+        mint
+    },
+
+    mounted() {
+        document.getElementById('mint').addEventListener('click', async () => {
+            if(this.counter != 0) {
+                await mint(this.counter);
+            } else {
+                document.getElementById('warningMessage').innerHTML = 'You must mint at least 1 NFT';
+            }
+        });
+    }
+}
+
+
+function increment() {
+    if(this.counter < 5) {
+        this.counter++;
+    }
+
+    if(this.counter == 5) {
+        const warningMessage = document.getElementById('warningMessage');
+        warningMessage.innerHTML = 'You can only increment the counter up to 5';
+        console.log('You can only increment the counter up to 5');
+    }
+}
+
+function decrement() {
+    const warningMessage = document.getElementById('warningMessage');
+    if(this.counter > 0) {
+        warningMessage.innerText = '';    
+        this.counter--;
+    }
+}
+
+
+</script>
+
